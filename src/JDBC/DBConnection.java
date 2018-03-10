d16ba395145dfc0f6b4b79be3400fb3ac88066e0
Hamelina <ehamelojulia@gmail.com>
2018-03-10 22:55:52 +0100

    Javadoc

--- src/JDBC/DBConnection.java
+++ src/JDBC/DBConnection.java
@@ -9,26 +9,90 @@ import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
- * @version 1.0
- * @author Fabien
+ * This class is handles the connection to the database.
+ * @author Alexandre Bottero
+ * @author Hamelina Ehamelo
+ * @author Fabien Turgut
+ * @author Marion Rul
+ * @version March 2018
  */
 public class DBConnection {
     
+    //Class variables *************************************************
+    
+    /**
+   * The host of the database.
+   */
     private static final String HOST = "localhost";
+    
+    /**
+   * The port to connect on.
+   */
     private static final String PORT = "5432";
+    
+    /**
+   * The name of the database to which we need to connect to.
+   */
     private static final String DATABASE = "";
+    
+    /**
+   * The name of the user.
+   */
     private static final String USERNAME = "";
+    
+    /**
+   * The password of the user to connect to the database.
+   */
     private static final String PASSWORD = "";
     
+    /**
+   * The Statement used for executing a static SQL statement and returning the results it produces
+   */
     private static Statement stmt = null;
+    
+    /**
+   * The Connection to the database.
+   */
     private static Connection c = null;
     
+     //Instance variables **********************************************
+  
+    /**
+     * The instance of the Java Database Connection corresponding to the host.
+     */
     private final String host;
+    
+    /**
+     * The instance of the Java Database Connection corresponding to port to connect to.
+     */
     private final String port;
+    
+    /**
+     * The instance of the Java Database Connection corresponding to the name of the database.
+     */
     private final String database;
+    
+    /**
+     * The instance of the Java Database Connection corresponding to the name of the user.
+     */
     private final String username;
+    
+    /**
+     * The instance of the Java Database Connection corresponding to the password of the user to connect to the database.
+     */
     private final String password;
     
+     //Constructors ****************************************************
+
+  /**
+   * Constructs an instance of the ClientConsole UI.
+   *
+   * @param host The host to connect to.
+   * @param port The port to connect on.
+   * @param database The database to connect to.
+   * @param username The username to connect with. 
+   * @param password The password to connect with. 
+   */
     public DBConnection(String host, String port, String database, String username, String password){
         this.host = host;
         this.port = port;
@@ -36,6 +100,7 @@ public class DBConnection {
         this.username = username;
         this.password = password;
     }
+    
     
     public DBConnection(){
         this.host = HOST;
