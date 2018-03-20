e653835f0df2499f29c22b34a3dd9bd67078c896
Unknown <ehamelojulia@gmail.com>
2018-03-20 10:02:52 +0100

    script sql créa User
    création d'un fichier de script de la création d'un user

---
+++ creaUser.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE public."user";
+
+CREATE TABLE public.User(
+	idUser SERIAL NOT NULL ,
+	mail   VARCHAR (25)  ,
+	pwd    VARCHAR (25)  ,
+	CONSTRAINT prk_constraint_User PRIMARY KEY (idUser)
+)WITHOUT OIDS;
