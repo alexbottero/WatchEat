004d3f68ba7ea723e04558f4709c9364a2b24f08
Fabien <fabien.turgut@etu.umontpellier.fr>
2018-03-26 10:59:07 +0200

    Correction BD recipe

--- src/BL/Recipe.java
+++ src/BL/Recipe.java
@@ -11,18 +11,20 @@ package BL;
  */
 public class Recipe extends Consumable{
 
-    private final String description;
-    private final String instructions;
-    private final String time;
-    private final String peopleAmount;
+    private String description;
+    private String instructions;
+    private String time;
+    private String peopleAmount;
+    private String type;
     
 
-    public Recipe(String name, String description, String instructions, String time, String peopleAmount) {
+    public Recipe(String name, String description, String instructions, String time, String peopleAmount, String type) {
         super(name);
         this.description = description;
         this.instructions = instructions;
         this.time = time;
         this.peopleAmount = peopleAmount;
+        this.type = type;
     }
     
 }
--- src/DAO/PostgresConsumableDAO.java
+++ src/DAO/PostgresConsumableDAO.java
@@ -31,20 +31,21 @@ public class PostgresConsumableDAO implements ConsumableDAO {
     public ArrayList<Consumable> getConsumables(){
         ArrayList<Consumable> consumables = new ArrayList<>();
         try {
-            String query = "SELECT * FROM public.consumable";
+            String query = "SELECT DISTINCT name FROM public.food f, public.consumable c WHERE f.idconsumable = f.idconsumable";
             ResultSet res = jdbc.select(query);
             while(res.next()){
                 Food food  = new Food(res.getString("name")); 
                 consumables.add(food);
             }
-            query = "SELECT * FROM public.recipe";
+            query = "SELECT * FROM public.recipe r, public.type t, public.consumable c WHERE r.idtype = t.idtype AND r.idconsumable = c.idconsumable";
             res = jdbc.select(query);
             while(res.next()){
                 Recipe recipe  = new Recipe(res.getString("name"),
                         res.getString("description"),
                         res.getString("instructions"),
                         res.getString("timeRecipe"),
-                        res.getString("peopleAmount")); 
+                        res.getString("peopleAmount"),
+                        res.getString("title")); 
                 consumables.add(recipe);
             }
         } catch (SQLException ex) {
