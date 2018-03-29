d31836d919c2fb29914aa863518b0c23228b74ab
Unknown <marion.mary@hotmail.fr>
2018-03-29 15:48:54 +0200

    Page terminée

--- src/UI/FXMLUserMenuManagementController.java
+++ src/UI/FXMLUserMenuManagementController.java
@@ -10,12 +10,18 @@ import BL.Recipe;
 import Facade.MenuFacade;
 import Helpers.NavigationHelpers;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -31,6 +37,9 @@ public class FXMLUserMenuManagementController implements Initializable {
     private Button myProposals_button;
     private MenuFacade menuFacade;
     private ArrayList<Menu> menus;
+    @FXML
+    private GridPane menus_GridPane;
+    DateFormat df = new SimpleDateFormat("dd-MM-yyyy", Locale.FRENCH);
     
     /**
      * Initializes the controller class.
@@ -40,6 +49,26 @@ public class FXMLUserMenuManagementController implements Initializable {
         navHelpers = new NavigationHelpers();
         menuFacade = new MenuFacade();
         menus = menuFacade.getMenus();
+        initMenus();
+    }
+
+    private void initMenus() {
+        int index = 1;
+        for(Menu menu : menus){
+            String nameCreator = menu.getCreator().getFirstName() + " " + menu.getCreator().getLastName().substring(0, 1);
+            menus_GridPane.add(new Label(menu.getName()),0,index);
+            menus_GridPane.add(new Label(menu.getDescription()),1,index);
+            menus_GridPane.add(new Label(df.format(menu.getDate())),2,index);
+            menus_GridPane.add(new Label(nameCreator),3,index);
+            Button seeButton = new Button("See");
+            seeButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    navHelpers.changeScene(homePage_button, "MyMenus", menu);
+                }
+            });
+            menus_GridPane.add(seeButton,4,index);
+            index++;
+        }
     }    
 
     @FXML
